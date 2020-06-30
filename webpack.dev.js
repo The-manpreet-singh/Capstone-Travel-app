d7140c9c5fb02da2d93845ea0ff5874425c23c34
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-30 20:46:08 +0530

    add webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -37,15 +37,15 @@ module.exports = {
 			template: "./src/client/views/index.html",
 			filename: "./index.html",
 		}),
-		// new CleanWebpackPlugin({
-		// 	// Simulate the removal of files
-		// 	dry: true,
-		// 	// Write Logs to Console
-		// 	verbose: true,
-		// 	// Automatically remove all unused webpack assets on rebuild
-		// 	cleanStaleWebpackAssets: true,
-		// 	protectWebpackAssets: false,
-		// }),
+		new CleanWebpackPlugin({
+			// Simulate the removal of files
+			dry: true,
+			// Write Logs to Console
+			verbose: true,
+			// Automatically remove all unused webpack assets on rebuild
+			cleanStaleWebpackAssets: true,
+			protectWebpackAssets: false,
+		}),
 		new WorkboxPlugin.GenerateSW(),
 	],
 };
