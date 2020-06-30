66dd27257994e9545aad7136e97203cb9a21b5ad
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-30 21:43:56 +0530

    set data object

--- src/client/index.js
+++ src/client/index.js
@@ -26,3 +26,20 @@ const dateFormat = (date) => {
 	const dateFormat = moment(date).format("LL");
 	return dateFormat;
 };
+
+const data = {};
+
+const saveTrips = async (e) => {
+	e.preventDefault();
+	data.city = cityValue();
+	const geocity = await geoData(data.city);
+	data.countryCode = geocity.countryCode;
+	const countryInfo = await countryData(data.countryCode);
+	data.country = countryInfo.name;
+	data.image = await imageData(data.city, data.country);
+	data.start = depart();
+	data.end = returnDate();
+	data.weatherForecast = await weatherData(data.city, data.country);
+	console.log(data);
+	recentTrip(data);
+};
