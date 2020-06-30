4e59a09a66f3dbe1cdaf026c1f71e966b63b4e25
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-30 21:34:02 +0530

    add weatherbit api

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,4 +1,8 @@
-import { geoData } from './../apis/geoNamesApi';
+import { geoData } from "./../apis/geoNamesApi";
+import { imageData } from "./../apis/pixabayApi";
+import { weatherData } from "./../apis/weatherApi";
+import { countryData } from "./../apis/restCountriesApi";
 
+//alert(hey);
 
-export { geoData }
+export { geoData, imageData, weatherData, countryData };
