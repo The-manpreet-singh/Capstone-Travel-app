57c4c285ff9fccd3cc2f183c93d5b8a9ca16832e
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-30 21:38:41 +0530

    set depart date

--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,12 @@ import { geoData, imageData, weatherData, countryData } from "./js/app";
 console.log(geoData());
 
 const cityValue = () => {
-  let city = document.getElementById('city').value;
-  if (typeof city !== 'string') return '';
-  return city[0].toUpperCase() + city.slice(1);
-}
+	let city = document.getElementById("city").value;
+	if (typeof city !== "string") return "";
+	return city[0].toUpperCase() + city.slice(1);
+};
+
+const depart = () => {
+	const date = document.getElementById("departure_date").value;
+	return date;
+};
