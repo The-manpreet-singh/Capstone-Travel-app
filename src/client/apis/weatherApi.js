530720710d08c0e29d1971365576fd516f28d89f
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-01 08:11:23 +0530

    fix errors

--- src/client/apis/weatherApi.js
+++ src/client/apis/weatherApi.js
@@ -1,13 +1,10 @@
-const countryData = async function (countryCode) {
-	const url = `https://restcountries.eu/rest/v2/alpha/${countryCode}`;
+export async function weatherData(city) {
+	const url = "https://api.weatherbit.io/v2.0/forecast/daily?" + `city=${city}&key=b3432a956d7b45a18ba374d67c837a2e`;
 	try {
 		const res = await fetch(url);
 		const data = await res.json();
-		return {
-			name: data.name,
-		};
+		return data;
 	} catch (error) {
 		console.log(error);
 	}
-};
-export { countryData };
+}
