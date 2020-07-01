fbcecd8e7c593c3bb7273ad79c6271187a9fb07e
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-01 07:59:26 +0530

    add inner html

--- src/client/index.js
+++ src/client/index.js
@@ -32,6 +32,45 @@ const daysRemainForTrip = (startDate, date) => {
 	return Math.ceil(difference / 86400000);
 };
 
+const recentTrip = (tripPlaner) => {
+  
+  const tripDeparture = dateFormat(tripPlaner.start);
+  const tripReturn = dateFormat(tripPlaner.end);
+  const difference = daysRemainForTrip(new Date(), tripPlaner.start);
+  const weather = weatherForecastData(tripPlaner.weatherForecast, difference, tripDeparture);
+
+  const div = document.createElement('div');
+   div.classList.add('save-trip');
+  document.querySelector('.trips-container').appendChild(div);
+
+  div.innerHTML = `
+   <div class="row">
+  <div class="col-sm-6 col-md-8">
+      <div class="card">
+          <div class="row">
+              <div class="col-md-6">
+                  <img src="${tripPlaner.image}" class="card-img-top" alt="...">
+                 </div>
+                 <div class="col-md-6">
+                  <div class="card-body">
+                      <h5 class="card-title">My Trip to: <span style="color:#02b3e4 ">${tripPlaner.city}, ${tripPlaner.country} </span></h5>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Departing: <span style="color:crimson"> ${tripDeparture}</span></li>
+                      <li class="list-group-item">Return:<span style="color:crimson"> ${tripReturn}</span></li>
+                      <li class="list-group-item">${tripPlaner.city}, ${tripPlaner.country} trip <span style="color:crimson"> ${daysRemainForTrip(new Date(), tripPlaner.start)}</span> days aways </li>
+                      <li class="list-group-item">Typical Weather temperature is <span style="color:crimson">${weather.temperature}&deg;F</span></li>
+                      <li class="list-group-item">weather forecate:  <span style="color:crimson">${weather.summary}</span> </li>
+                    </ul>
+                 </div>
+          </div>
+        </div>
+  </div>
+</div> `
+  ;
+}
+
+
 const weatherForecastData = (weatherForecast, difference) => {
 	const weather = {
 		temperature: 0,
